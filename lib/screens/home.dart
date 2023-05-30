e82256ab6b8af711661497ef5b7105cc08683f54
Merve Ağaçayak <mrve.agcyk@gmail.com>
2023-05-30 14:06:14 +0300

    creating home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -22,6 +23,17 @@ class _HomePageState extends State<HomePage> {
         title: const Text(
           "PDF READER",
           style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text("Recent Documents",
+                style: GoogleFonts.roboto(
+                    fontSize: 25.0, fontWeight: FontWeight.bold)),
+          ],
         ),
       ),
     );
